ed564cb88bb7d490d8a14abeeac6126bcbdc332b
fabioportovasques <fabioportovasques321@gmail.com>
2021-07-28 20:38:42 -0300

    altercao titulo arquivo cad-tipo-veiculo

--- dashboard/dist/cad-tipo-veiculo.php
+++ dashboard/dist/cad-tipo-veiculo.php
@@ -29,7 +29,7 @@ if(empty($_SESSION['lg'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Cadastrar tipo de veículo</title>
+        <title>Cadastrar Marcas/Modelos de veículo</title>
         
         <link href="css/styles.css" rel="stylesheet"/>
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
@@ -284,7 +284,7 @@ if(empty($_SESSION['lg'])) {
     <br />
  
 
-                                           <h2 class="text-center">Cadastrar Tipo de Veículo</h2><br />
+                                           <h2 class="text-center">Cadastrar Marcas/Modelos de Veículo</h2><br />
 
                                            <hr>
                                            
